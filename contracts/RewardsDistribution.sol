ff622951ad7dbbdf9d0559f1e950e48a765dfb9f
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-03-01 01:18:23 +0100

    Added random number generator

--- contracts/RewardsDistribution.sol
+++ contracts/RewardsDistribution.sol
@@ -2,19 +2,18 @@
 pragma solidity ^0.8.24;
 
 import "./errors/Errors.sol";
+import "./VRFv2Consumer.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-// import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
-contract RewardsDistribution {
+contract RewardsDistribution is VRFv2Consumer {
     address public rewardsToken;
-    address public owner;
+    address _owner;
     address public gameAddress;
 
+    uint32 public entryToDistribution;
     uint256 public totalReward;
-    uint256 public entryToDistribution;
 
     mapping(address => bool) public isRegistered;
     mapping(address => bool) public isEligible;
@@ -26,11 +25,11 @@ contract RewardsDistribution {
 
     event EligibleUserAdded(address userAddress, uint256 entryNo);
     event TotalRewardUpdated(uint256 _from, uint256 _to);
-    event RewardSent(address indexed _user, uint256 _amount);
+    event RewardSent(uint256 userNumber, address indexed _user, uint256 _amount);
     event UpdateGameAddress(address _newAddress);
 
-    constructor(address _rewardsToken, uint256 _amountToDistribution) {
-        owner = msg.sender;
+    constructor(address _rewardsToken, uint32 _amountToDistribution, uint64 subscriptionID) VRFv2Consumer (subscriptionID) {
+        _owner = msg.sender;
         rewardsToken = _rewardsToken;
         isRegistered[msg.sender] = true;
         entryToDistribution = _amountToDistribution;
@@ -72,10 +71,14 @@ contract RewardsDistribution {
 
             userEntries[_user] = userEntries[_user] + 1;
         }
+
+        if (eligibleUsers.length == entryToDistribution) {
+            requestRandomWords(entryToDistribution);
+        }
     }
 
     function setGameAddress(address _gameAddress) public {
-        if (msg.sender != owner) {
+        if (msg.sender != _owner) {
             revert Errors.NOT_OWNER();
         }
 
@@ -85,7 +88,7 @@ contract RewardsDistribution {
     }
 
     function changeTotalReward(uint256 _newTotalReward) public {
-        if (msg.sender != owner) {
+        if (msg.sender != _owner) {
             revert Errors.NOT_OWNER();
         }
 
@@ -95,8 +98,8 @@ contract RewardsDistribution {
         emit TotalRewardUpdated(_oldReward, _newTotalReward);
     }
 
-    function changeEntryToDistribution(uint256 _newEntry) public {
-        if (msg.sender != owner) {
+    function changeEntryToDistribution(uint32 _newEntry) public {
+        if (msg.sender != _owner) {
             revert Errors.NOT_OWNER();
         }
 
@@ -106,8 +109,7 @@ contract RewardsDistribution {
         emit TotalRewardUpdated(_oldEntry, _newEntry);
     }
 
-    // TODO: change to internal
-    function _calculateRewardPerUser() public view returns (uint256 _reward) {
+    function _calculateRewardPerUser() internal view returns (uint256 _reward) {
         if (entryToDistribution == 0) {
             revert();
         }
@@ -115,7 +117,7 @@ contract RewardsDistribution {
     }
 
     function distributeRewards() public {
-        if (msg.sender != owner) {
+        if (msg.sender != _owner) {
             revert Errors.NOT_OWNER();
         }
 
@@ -131,10 +133,17 @@ contract RewardsDistribution {
     function _shareRewards(uint256 _reward) internal {
         IERC20 _rewardContract = IERC20(rewardsToken);
 
-        for (uint256 i = 0; i < eligibleUsers.length; i++) {
-            require(_rewardContract.transfer(eligibleUsers[i], _reward), "could not send transaction");
+        (bool _fullfiled, uint256[] memory _randomNumbers) = getRequestStatus(lastRequestId);
 
-            emit RewardSent(eligibleUsers[i], _reward);
+        if (!_fullfiled) {
+            revert("Random numbers not generated");
+        }
+
+        for (uint256 i = 0; i < eligibleUsers.length; i++) {
+            uint256 userNumber = (_randomNumbers[i] % entryToDistribution) + 1;
+            require(_rewardContract.transfer(eligibleUsers[userNumber], _reward), "could not send transaction");
+
+            emit RewardSent(userNumber, eligibleUsers[userNumber], _reward);
         }
     }
 }
--- contracts/errors/Errors.sol
+++ contracts/errors/Errors.sol
@@ -2,17 +2,10 @@
 pragma solidity ^0.8.24;
 
 library Errors {
-    error ZERO_ADDRESS_CALL();
-    error ONLY_ADMIN_HAVE_ACCESS();
-    error ONLY_MODEARTOR_AND_ADMIN_HAVE_ACCESS();
-    error UNREGISTERED_ADDRESS();
-    error NOT_A_MODERATOR();
+    error ADDRESS_ZERO_CALL();
     error USER_ALREADY_REGISTERED();
-    error CANNOT_JOIN_COMMUNITY_TWICE();
-    error NO_USER_POST();
-    error ONLY_OWNER();
-    error NO_SOCIAL_MEDIA_CREATED();
-    error INVALID_COMMUNITY_ID();
-    error ONLY_COMMUNITY_MEMBER();
-    error CANNOT_ASSIGN_MODERATOR_TWICE();
+    error USER_NOT_REGISTERED();
+    error NOT_GAME_ADDRESS();
+    error NOT_OWNER();
+    error TOTAL_ENTRIES_NOT_REACHED();
 }
